4d2b22e72cb0c966b13b419a90a8588946e75cd3
Sviluppo 2 <sviluppo2@Sviluppos-MacBook-Pro.local>
2025-11-11 11:14:37 +0100

    avanza f

--- wp-content/themes/hello-elementor-child/assets/scripts/script.js
+++ wp-content/themes/hello-elementor-child/assets/scripts/script.js
@@ -148,7 +148,7 @@ document.addEventListener('wpcf7mailsent', function (event) {
                 });
             }, 500); // deve corrispondere a transition: 0.6s
 
-        }, 1000); // tempo di attesa prima del fade-out, in ms
+        }, 500); // tempo di attesa prima del fade-out, in ms
 
 
     }
@@ -181,7 +181,7 @@ document.addEventListener('wpcf7mailsent', function (event) {
                 });
             }, 500); // deve corrispondere a transition: 0.6s
 
-        }, 1000); // tempo di attesa prima del fade-out, in ms
+        }, 500); // tempo di attesa prima del fade-out, in ms
 
     }
     document.querySelector('.button-container-box')?.classList.add('justify-content-center');
